46585c2c56b13c8c6009f85785b6314635452c98
jay <jay@marsmatics.com>
2024-03-11 14:25:34 -0400

    Added screens.

---
+++ src/screens/Profile.tsx
@@ -0,0 +1,12 @@
+import {View} from '@gluestack-ui/themed';
+import Header from '../components/Header';
+
+const Profile = () => {
+  return (
+    <View>
+      <Header title="Profile" />
+    </View>
+  );
+};
+
+export default Profile;
