7575dffc55f8f2538a0191c7e0fc0af9189a81da
marsmatics-vito <vito@marsmatics.com>
2024-03-13 12:04:14 -0400

    update: welcome page and navigation

--- src/components/Slider/SlideItem.tsx
+++ src/components/Slider/SlideItem.tsx
@@ -12,32 +12,13 @@ import React from 'react';
 const {width, height} = Dimensions.get('screen');
 
 const SlideItem = ({item}: any) => {
-  const translateYImage = new Animated.Value(40);
-
-  Animated.timing(translateYImage, {
-    toValue: 0,
-    duration: 1000,
-    useNativeDriver: true,
-    easing: Easing.bounce,
-  }).start();
-
   return (
     <View style={styles.container}>
-      <Animated.Image
-        source={item.img}
+      <Image
+        style={styles.image}
+        source={item.image == '' ? '' : item.image}
         resizeMode="contain"
-        style={[
-          styles.image,
-          {
-            transform: [
-              {
-                translateY: translateYImage,
-              },
-            ],
-          },
-        ]}
       />
-
       <View style={styles.content}>
         <Text style={styles.title}>{item.title}</Text>
         <Text style={styles.description}>{item.description}</Text>
@@ -50,15 +31,16 @@ export default SlideItem;
 
 const styles = StyleSheet.create({
   container: {
-    width,
+    width: width,
+    height: '100%',
     alignItems: 'center',
+    justifyContent: 'space-around',
   },
   image: {
     flex: 0.6,
     width: '90%',
   },
   content: {
-    flex: 0.4,
     alignItems: 'center',
     width: '90%',
   },
@@ -67,13 +49,13 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#333',
     width: '90%',
-    textAlign:"center",
+    textAlign: 'center',
   },
   description: {
     fontSize: 18,
     marginVertical: 12,
     color: '#333',
-    textAlign:"center",
+    textAlign: 'center',
   },
   price: {
     fontSize: 32,
